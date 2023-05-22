c17ab51ee7115b35dfd92b98f1cf1a2bc73dc432
Christopher Sasias <102446378+ChrisSasias@users.noreply.github.com>
2023-05-22 18:07:26 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#phrase").innerHTML = generateDomain();
+};
+
+const generateDomain = () => {
+  const prount = ["our", "theskinnye", "thedoortothe"];
+  const what = ["dream", "nightmare", "goal"];
+  const adj = ["great", "big", "incredible"];
+  const domain = [".com", ".org", ".uy", ".es", ".dev"];
+
+  const getRandomDomain = arr => arr[Math.floor(Math.random() * arr.length)];
+
+  return `${getRandomDomain(prount)}${getRandomDomain(what)}${getRandomDomain(
+    adj
+  )}${getRandomDomain(domain)}`;
 };
